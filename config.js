1b01016a17c5f876df59176781effa5e8155380f
jsngn <mien.t.nguyen.22@dartmouth.edu>
2020-05-18 21:05:37 -0400

    Credentials

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+sunapee: {
+	database: {
+		host     : 'sunapee.cs.dartmouth.edu',
+		user     : 'OceansOfPotions_sp20', //'your sunapee username here'
+		password : 'DeB%9?8M', //'your sunapee password here'
+		schema : 'OceansOfPotions_sp20' //'your sunapee default schema'
+	},
+	port: 8080
+}
+};
+module.exports = config;
