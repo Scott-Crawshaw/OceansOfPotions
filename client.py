f8b336af49141f532831b9bf23922327c2bb755a
Joanna Liu <joanna.y.liu.22@dartmouth.edu>
2020-05-22 03:42:10 -0700

    Added create/update/delete customer

--- client.py
+++ client.py
@@ -16,7 +16,7 @@ def make_get_call(url):
 # Returns true if successful and false if failed. Prints error message.
 def make_post_call(url, data):
     resp = requests.post(url, json=data)
-    if resp.json()['status'] != 201:
+    if resp.json()['status'] != 200:
         # Make sure not to expose SQL by checking if response is a dict before printing error message
         if type(resp.json()['error']) == dict and 'sqlMessage' in resp.json()['error']:
             print("Error " + str(resp.json()['status']) + ": " + str(resp.json()['error']['sqlMessage']))
@@ -52,96 +52,129 @@ def make_delete_call(url):
 
 if __name__ == '__main__':
     while True:
-        has_account = input("Press 1 if you have an account, press 2 if you would like to make one: ")
-        if has_account == '2': # CREATE AN ACCOUNT
-            username = input("Enter your username: ")
-            while not username:
-                username = input("Please reenter a non-null username: ")
+        print("\nUse one of the following commands to choose a part of the database to view/change, or QUIT to quit:")
+        print("CUSTOMERS - This command lets you create, view, or update customer accounts")
+        print("FOLLOWINGS - This command lets you view following/follower lists and update who you follow")
+        print("ORDERS - This command lets you view your own orders and those of people you are following, and create or add to your orders")
+        print("POTIONS - This command lets you view available potions")
 
-            password = input("Enter your password: ")
-            while not password:
-                password = input("Please reenter a non-null password: ")
+        inputCommand = input("Type your command here: ")
+        if inputCommand == "QUIT":
+            break
 
-            fname = input("Enter your first name: ")
-            while not fname:
-                fname = input("Please reenter a non-null first name: ")
+        if inputCommand == "CUSTOMERS":
+            hasAccount = input("Do you have an account? (Y/N): ")
+            if hasAccount == "N":
+                username = input("To create an account, enter your username: ")
+                while not username:
+                    username = input("Please reenter a non-null username: ")
 
-            minitial = input("Enter your middle initial: ")
-            while len(minitial) > 1:
-                minitial = input("Please reenter a valid middle initial: ")
+                password = input("Enter your password: ")
+                while not password:
+                    password = input("Please reenter a non-null password: ")
 
-            lname = input("Enter your last name: ")
-            while not lname:
-                lname = input("Please reenter a non-null last name: ")
+                fname = input("Enter your first name: ")
+                while not fname:
+                    fname = input("Please reenter a non-null first name: ")
 
-            dob = input("Enter your date of birth (YYYY-MM-DD): ")
-            while not dob:
-                dob = input("Please reenter a non-null date of birth: ")
+                minitial = input("Enter your middle initial: ")
+                while len(minitial) > 1:
+                    minitial = input("Please reenter a valid middle initial: ")
 
-            email = input("Enter your email: ")
-            while not email:
-                email = input("Please reenter a non-null email: ")
+                lname = input("Enter your last name: ")
+                while not lname:
+                    lname = input("Please reenter a non-null last name: ")
 
-            phone = input("Enter your phone number: ")
-            while not phone:
-                phone = input("Please reenter a non-null phone number: ")
+                dob = input("Enter your date of birth (YYYY-MM-DD): ")
+                while not dob:
+                    dob = input("Please reenter a non-null date of birth: ")
 
-            data = {"user": username, "password": password, "fname": fname, "minitial": minitial, "lname": lname, "dob": dob, "email": email, "phone": phone}
-            if make_post_call('http://localhost:8080/customers/', data):
-                print("Account created successfully!")
-            else:
-                print("Account creation unsuccessful: please try again!")
+                email = input("Enter your email: ")
+                while not email:
+                    email = input("Please reenter a non-null email: ")
 
-        elif has_account == '1': # ALREADY HAVE AN ACCOUNT
-            customer_username = input("Please enter your username: ")
-            customer_password = input("Please enter your password: ")
+                phone = input("Enter your phone number: ")
+                while not phone:
+                    phone = input("Please reenter a non-null phone number: ")
 
-            while True:
-                action = input("What would you like to do? Press 1 to view/update/delete your own account, press 2 to view all customers, press 3 to view/update followings, press 4 to view potions, press 5 to create an order: ")
-                if action == '1': # VIEW/UPDATE/DELETE YOUR OWN ACCOUNT
-                    option1 = input("Press 1 to view your account, press 2 to delete your account, press 3 to update your account, press 4 to update your password: ")
-                    while option1 not in ['1', '2', '3', '4']:
-                        print("Invalid input: please reenter!")
-                        option1 = input("Press 1 to view your account, press 2 to delete your account, press 3 to update your account, press 4 to update your password: ")
-                    if option1 == '1': # VIEW YOUR ACCOUNT
-                        pass
-                    elif option1 == '2': # DELETE YOUR ACCOUNT
-                        pass
-                        # remember: need to break out of the loop here
-                    elif option1 == '3': # UPDATE YOUR ACCOUNT
-                        pass
-                        # remember: need to break out of the loop here if username changes
-                    elif option1 == '4': # UPDATE YOUR PASSWORD
-                        pass
-                        # remember: need to break out of the loop here
-                elif action == '2': # VIEW ALL CUSTOMERS
-                    pass
-                elif action == '3': # VIEW/UPDATE FOLLOWINGS
-                    option3 = input("Press 1 to follow another customer, press 2 to unfollow another customer, press 3 to view who you are following, press 4 to view your followers: ")
-                    while option3 not in ['1', '2', '3', '4']:
-                        print("Invalid input: please reenter!")
-                        option3 = input("Press 1 to follow another customer, press 2 to unfollow another customer, press 3 to view who you are following, press 4 to view your followers: ")
-                    if option3 == '1': # FOLLOW ANOTHER CUSTOMER
-                        pass
-                    elif option3 == '2': # UNFOLLOW ANOTHER CUSTOMER
-                        pass
-                    elif option3 == '3': # VIEW WHO YOU ARE FOLLOWING
-                        pass
-                    elif option3 == '4': # VIEW WHO IS FOLLOWING YOU
-                        pass
-                elif action == '4': # VIEW POTIONS
-                    option4 = input("Press 1 to get all potions, press 2 to get a single potion: ")
-                    while option4 not in ['1', '2']:
-                        print("Invalid input: please reenter!")
-                        option4 = input("Press 1 to get all potions, press 2 to get a single potion: ")
-                    if option4 == '1': # VIEW ALL POTIONS
-                        pass
-                    elif option4 == '2': # VIEW A SINGLE POTION
-                        pass
-                elif action == '5': # CREATE AN ORDER
-                    pass
+                data = {"user": username, "password": password, "fname": fname, "minitial": minitial, "lname": lname, "dob": dob, "email": email, "phone": phone}
+                if make_post_call('http://localhost:8080/customers/', data):
+                    print("Account created successfully!")
                 else:
-                    print("Invalid input: Please reenter.")
+                    print("Account creation unsuccessful: please try again!")
 
-        else:
-            print("Invalid input: Please reenter.")
+            elif hasAccount == "Y":
+                print("Please provide your login information.")
+                loginUsername = input("Username: ")
+                loginPassword = input("Password: ")
+
+                print("\nUse one of the following commands to access the database:")
+                print("DELETE - This command lets you delete your own account")
+                print("UPDATE - This command lets you update account details")
+                print("VIEW - This command lets you view customer account details")
+
+                customerCommand = input("Type your command here: ")
+
+                if customerCommand == "DELETE":
+                    if make_delete_call('http://localhost:8080/customers?user=%s&pw=%s' % (loginUsername, loginPassword)):
+                        print("Account deleted successfully!")
+                    else:
+                        print("Account deletion unsuccessful: please try again!")
+
+                elif customerCommand == "UPDATE":
+                    print("What attribute would you like to update?")
+                    print("Options: USERNAME, PASSWORD, FIRST NAME, MIDDLE INITIAL, LAST NAME, DOB, EMAIL, PHONE")
+                    attrToUpdate = input("Type your command here: ")
+
+                    if attrToUpdate == "PASSWORD":
+                        newPassword = input("New password: ")
+                        if make_put_call('http://localhost:8080/customers/password?user=%s&pw=%s' % (loginUsername, loginPassword), {"newPassword": newPassword}):
+                            print("Password updated successfully!")
+                        else:
+                            print("Password update failed: please try again!")
+
+                    else:
+                        if attrToUpdate == "USERNAME":
+                            attribute = "CustomerUsername"
+                            newValue = input("New username: ")
+
+                        elif attrToUpdate == "FIRST NAME":
+                            attribute = "CustomerFirstName"
+                            newValue = input("New first name: ")
+
+                        elif attrToUpdate == "LAST NAME":
+                            attribute = "CustomerLastName"
+                            newValue = input("New last name: ")
+
+                        elif attrToUpdate == "MIDDLE INITIAL":
+                            attribute = "CustomerMiddleInitial"
+                            newValue = input("New middle initial: ")
+
+                        elif attrToUpdate == "DOB":
+                            attribute = "CustomerDOB"
+                            newValue = input("New date of birth (YYYY-MM-DD): ")
+
+                        elif attrToUpdate == "EMAIL":
+                            attribute = "CustomerPrimaryEmail"
+                            newValue = input("New email: ")
+
+                        elif attrToUpdate == "PHONE":
+                            attribute = "CustomerPrimaryPhone"
+                            newValue = input("New phone number: ")
+
+                        if make_put_call('http://localhost:8080/customers?user=%s&pw=%s' % (loginUsername, loginPassword), {"attribute": attribute, "newValue": newValue}):
+                            print("Updated " + attrToUpdate + " successfully")
+                        else:
+                            print("Update failed: please try again!")
+
+                elif customerCommand == "VIEW":
+                    pass
+
+        elif inputCommand == "FOLLOWINGS":
+            pass
+
+        elif inputCommand == "ORDERS":
+            pass
+
+        elif inputCommand == "POTIONS":
+            pass
